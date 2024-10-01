79ce65b795ed5bfdb65bd32727874fee32bafab4
TaylorOkis <taylorokis@yahoo.com>
2024-10-01 12:27:50 +0100

    Exercise done.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (string) {
 
+    reversed_word = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversed_word += string[i];
+    }
+
+    return reversed_word;
 };
 
 // Do not edit below this line
