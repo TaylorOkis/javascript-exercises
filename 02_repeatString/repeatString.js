bb7ac7c497e14255c48967143cf701159212554a
TaylorOkis <taylorokis@yahoo.com>
2024-10-01 12:00:45 +0100

    Finish exercise number 2. All tests passed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word, number_of_times) {
+    let repeatWord = '';
+    if (number_of_times >= 0) {
+        for (let i = 0; i < number_of_times; i++) {
+            repeatWord += word;
+        }
+    } else {
+        return 'ERROR';
+    }
+    return repeatWord;
 };
 
 // Do not edit below this line
